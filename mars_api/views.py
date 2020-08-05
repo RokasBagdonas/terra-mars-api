f0f572326b83aedacdd6f2a639f6648c33b85b81
RokasBagdonas <rokius.bagdonas@gmail.com>
2020-08-05 09:01:59 +0100

    add Game model, serializer, view and routes.

--- mars_api/views.py
+++ mars_api/views.py
@@ -1,9 +1,13 @@
 from rest_framework import viewsets
 
-from mars_api.models import Player
-from mars_api.serializers import PlayerSerializer
+from mars_api.models import Game, Player
+from mars_api.serializers import GameSerializer, PlayerSerializer
 
 
 class PlayerViewSet(viewsets.ModelViewSet):
     queryset = Player.objects.all()
     serializer_class = PlayerSerializer
+
+class GameViewSet(viewsets.ModelViewSet):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
