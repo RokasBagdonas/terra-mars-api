0cbd010f06862f35440152e1e3fdf165c7352a70
RokasBagdonas <rokius.bagdonas@gmail.com>
2020-08-20 08:53:57 +0100

    temp

--- mars_api/serializers.py
+++ mars_api/serializers.py
@@ -16,6 +16,15 @@ class GameSerializer(serializers.ModelSerializer):
 
 
 class PlayerScoreSerializer(serializers.ModelSerializer):
+    player = PlayerSerializer()
     class Meta:
         model = PlayerScore
         fields = "__all__"
+
+    # How to skip unique "nickname" validation for the player?
+    def create(self, validated_data):
+        player_data = validated_data.pop("player")
+        player = Player.objects.get_or_create(**player_data)[0]
+
+        player_score = PlayerScore.objects.create(player=player, **validated_data)
+        return player_score
