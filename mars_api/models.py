92f68a8b83b6707980a52a10cd23a6f3db738fb4
RokasBagdonas <rokius.bagdonas@gmail.com>
2020-08-26 16:02:58 +0100

    GameAndPlayerScores serializer

--- mars_api/models.py
+++ mars_api/models.py
@@ -1,8 +1,90 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
+from model_utils import Choices
+
+CORPORATIONS = Choices(
+    "Aphrodite",
+    "Aridor",
+    "Arklight",
+    "Celestic",
+    "Cheung Shing Mars",
+    "Credicor",
+    "Ecoline",
+    "Helion",
+    "Interplanetary Cinematics",
+    "Inventrix",
+    "Manutech",
+    "Mining Guild",
+    "Morning Star Inc.",
+    "Phobolog",
+    "Point Luna",
+    "Polyphemos",
+    "Poseidon",
+    "Robinson Industries",
+    "Saturn Systems",
+    "Storm Craft Incorporated",
+    "Terractor",
+    "Tharsis Republic",
+    "Thorgate",
+    "United Nations Mars Initiative",
+    "Valley Trust",
+    "Viron",
+    "Vitor",
+)
+
+
+MAPS = Choices("Tharsis", "Elysium", "Hellas")
 
 
 class Player(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     nickname = models.CharField(max_length=32, unique=True)
     motto = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.nickname}"
+
+
+class Game(models.Model):
+    date = models.DateTimeField(default=timezone.now)
+    game_map = models.CharField(choices=MAPS, default=MAPS.Tharsis, max_length=14)
+
+    draft_variant = models.BooleanField(default=True)
+    prelude = models.BooleanField(default=False)
+    venus_next = models.BooleanField(default=False)
+    colonies = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"id: {self.id}; date: {self.date}; game_map: {self.game_map}"
+
+
+class PlayerScore(models.Model):
+    player = models.ForeignKey(Player, models.SET_NULL, related_name="player_scores", null=True)
+    game = models.ForeignKey(Game, models.CASCADE, related_name="player_scores")
+    corporation = models.CharField(choices=CORPORATIONS, max_length=40, blank=False, null=False)
+
+    terraform_rating = models.PositiveSmallIntegerField(default=20)
+    milestones = models.PositiveSmallIntegerField(default=0)
+    awards = models.PositiveSmallIntegerField(default=0)
+    greeneries = models.PositiveSmallIntegerField(default=0)
+    cities = models.PositiveSmallIntegerField(default=0)
+
+    event_cards = models.SmallIntegerField(default=0)
+    automated_cards = models.SmallIntegerField(default=0)
+    active_cards = models.SmallIntegerField(default=0)
+    resources = models.SmallIntegerField(default=0)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["player", "game"], name="one_score_per_player_per_game"
+            )
+        ]
+
+    def __str__(self):
+        s = self.player.nickname + "," if self.player else "<no player>"
+        return f"""player nickname: {s},
+        game_id: {self.game_id},
+        game date: {self.game.date},
+        corporation: {self.corporation}."""
--- mars_api/serializers.py
+++ mars_api/serializers.py
@@ -1,9 +1,99 @@
 from rest_framework import serializers
 
-from mars_api.models import Player
+from mars_api.models import Game, Player, PlayerScore
 
 
 class PlayerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Player
         fields = "__all__"
+
+
+class GameSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Game
+        fields = "__all__"
+
+
+class PlayerSerializerForScore(PlayerSerializer):
+    """
+       The difference between the default `PlayerSerializer` is that this one negates the
+       `unique` constraint of the nickname. This is because when creating a PlayerScore,
+        we want to have the option to create a Player if one doesn't exist.
+    """
+
+    nickname = serializers.CharField(max_length=32)
+
+    def create(self, validated_data):
+        instance, _ = Player.objects.get_or_create(**validated_data)
+        return instance
+
+
+class PlayerScoreSerializer(serializers.ModelSerializer):
+    player = PlayerSerializerForScore()
+
+    def create(self, validated_data):
+        player_data = validated_data.pop("player")
+        player, _ = Player.objects.get_or_create(**player_data)
+
+        player_score = PlayerScore.objects.create(player=player, **validated_data)
+        return player_score
+
+    class Meta:
+        model = PlayerScore
+        fields = "__all__"
+
+
+class PlayerScoreForGameSerializer(serializers.ModelSerializer):
+    player = PlayerSerializerForScore()
+
+    def create(self, validated_data):
+        player_data = validated_data.pop("player")
+        player, _ = Player.objects.get_or_create(**player_data)
+
+        player_score = PlayerScore.objects.create(player=player, **validated_data)
+        return player_score
+
+    class Meta:
+        model = PlayerScore
+        exclude = ["game"]
+
+
+class GameAndPlayersScoresSerializer(serializers.ModelSerializer):
+    """
+    Serializes a Game as well as PlayerScores.
+    """
+
+    players_scores = PlayerScoreForGameSerializer(many=True)
+
+    class Meta:
+        model = Game
+        fields = [
+            "date", "game_map", "draft_variant",
+            "prelude", "venus_next",
+            "colonies", "players_scores",
+        ]
+
+    def create(self, validated_data):
+        """
+        Issue 1: game has to be created before validating player scores.
+        However, the `PlayerScoreSerializer` is using the default `PlayerScore` model that requires Game.
+
+        Solutions:
+        1. Alter/Create PlayerScoreSerializer that does not have not null FK constraint so
+        that validation passes. Once validation passes, add the same game instance to each
+        PlayerScore and then save it to db.
+        """
+        # 1. create a game
+        players_scores = validated_data.pop("players_scores")
+
+        game = Game.objects.create(**validated_data)
+
+        # 2. create player_scores
+        players_scores_instances = []
+        for ps in players_scores:
+            players_scores_instances.append(PlayerScore.objects.create(**ps))
+
+        # 3. return
+        result = {"game": game, "players_scores": players_scores_instances}
+        return result
