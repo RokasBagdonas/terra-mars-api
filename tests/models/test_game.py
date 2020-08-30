ddace65dae26309d661e024477f26fc41d52f352
RokasBagdonas <rokius.bagdonas@gmail.com>
2020-08-30 13:07:21 +0100

    Game model test

--- tests/models/test_game.py
+++ tests/models/test_game.py
@@ -3,6 +3,8 @@ from django.db.utils import IntegrityError
 
 from mars_api.models import MAPS, Game
 
+from ..factories import PlayerScoreFactory
+
 pytestmark = pytest.mark.django_db
 
 
@@ -11,15 +13,16 @@ def test_game_cannot_be_created_with_invalid_game_map():
     with pytest.raises(IntegrityError):
         Game.objects.create(game_map="Invalid name")
 
+
 def test_game_can_be_created_with_valid_game_map():
     Game.objects.create(game_map="Tharsis")
 
 
-def test_game_returns_players_nicknames():
+def test_game_returns_related_players_scores(game):
     """When PlayerScores exist, check that corresponding Player nicknames are returned."""
 
-    # 1. Create PlayerScores. Sidenote: PlayerScores should be allowed to be created without an existing Player.
+    ps1 = PlayerScoreFactory(game=game)
+    ps2 = PlayerScoreFactory(game=game)
 
-    # 2. GET a Game
+    assert game.players_scores.count() == 2
 
-    # 3. assert if nicknames exist in game.players property.
