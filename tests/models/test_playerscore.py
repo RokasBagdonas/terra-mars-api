5d5ebbbd4b5fcba2ddf96474c8a007e285eacf38
RokasBagdonas <rokius.bagdonas@gmail.com>
2020-08-31 17:53:23 +0100

    refactor tests to use one factory.
    Uses one object factory to produce multiple instances instead of
    multiple factories to produce one instace.
    add `make` command for running a python shell in a docker container that
    has development dependencies (eg: testing).

--- tests/models/test_playerscore.py
+++ tests/models/test_playerscore.py
@@ -8,31 +8,31 @@ from ..factories import GameFactory, PlayerScoreFactory
 pytestmark = pytest.mark.django_db
 
 
-def test_can_create_multiple_player_scores_same_player(player):
-    ps1 = PlayerScoreFactory(player=player)
-    ps2 = PlayerScoreFactory(player=player)
+def test_can_create_multiple_player_scores_same_player(player, player_score_factory):
+    ps1 = player_score_factory(player=player)
+    ps2 = player_score_factory(player=player)
 
     assert PlayerScore.objects.filter(player=player).count() == 2
 
 
 
-def test_player_not_allowed_to_have_multiple_scores_in_the_same_game(game):
+def test_player_not_allowed_to_have_multiple_scores_in_the_same_game(game, player_score_factory):
     """
     Tests if only unique players can be added to the PlayerScore with same game_id.
     (So that there wouldn't be a player that has two scores for the same Game.)
     """
-    ps1 = PlayerScoreFactory(game=game)
+    ps1 = player_score_factory(game=game)
     with pytest.raises(IntegrityError):
-        ps2 = PlayerScoreFactory(corporation="Terractor", player=ps1.player, game=game)
+        ps2 = player_score_factory(corporation="Terractor", player=ps1.player, game=game)
 
 
-def test_same_player_in_different_games(player):
+def test_same_player_in_different_games(player, player_score_factory):
     """
     Tests if a Player is allowed to have multiple scores but in different games.
     Tested by not throwing an exception on the second PlayerScore creation.
     """
-    ps1 = PlayerScoreFactory(player=player)
-    ps2 = PlayerScoreFactory(player=player)
+    ps1 = player_score_factory(player=player)
+    ps2 = player_score_factory(player=player)
 
     assert ps1.player.nickname == ps2.player.nickname
     assert ps1.game != ps2.game
@@ -43,18 +43,18 @@ def test_cannot_create_player_score_with_invalid_corporation():
         PlayerScoreFactory(corporation="")
 
 
-def test_cannot_have_duplicate_corporations_same_game(game):
-    ps1 = PlayerScoreFactory(game=game)
+def test_cannot_have_duplicate_corporations_same_game(game, player_score_factory):
+    ps1 = player_score_factory(game=game)
 
     with pytest.raises(IntegrityError):
-        PlayerScoreFactory(game=game)
+        player_score_factory(game=game)
 
 
-def test_can_have_same_corporation_same_player_different_games(player):
+def test_can_have_same_corporation_same_player_different_games(player, game_factory, player_score_factory):
     """Prerequisite: PLayerScoreFactory'ies use the same corporation."""
     corporation = "Thorgate"
-    g1 = GameFactory()
-    ps1 = PlayerScoreFactory(game=g1, corporation=corporation, player=player)
+    g1 = game_factory()
+    ps1 = player_score_factory(game=g1, corporation=corporation, player=player)
 
-    g2 = GameFactory()
-    ps2 = PlayerScoreFactory(game=g2, corporation=corporation, player=player)
+    g2 = game_factory()
+    ps2 = player_score_factory(game=g2, corporation=corporation, player=player)
