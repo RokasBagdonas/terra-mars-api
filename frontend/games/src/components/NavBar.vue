d8cf9d7e421d725632f05887d34260f434266072
RokasBagdonas <rokius.bagdonas@gmail.com>
2020-09-30 12:22:22 +0100

    add Responsive burger navbar

--- frontend/games/src/components/NavBar.vue
+++ frontend/games/src/components/NavBar.vue
@@ -4,17 +4,20 @@
         <h1 class="navbar-item title is-3">
             <i class="icon fas fa-meteor"></i>
         </h1>
-        <a role="button" class="navbar-burger burger" aria-label="menu"
-        aria-expanded="false" data-target="navbarOptions">
+        <a role="button" class="navbar-burger" aria-label="menu"
+        aria-expanded="false" data-target="navbarOptions" v-on:click="toggleBurger">
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
         </a>
     </div>
-    <div id="navbarOptions" class="navbar-menu">
+    <div class="navbar-menu" id="navbarOptions">
         <div class="navbar-start">
-            <a class="navbar-item" href="">Home</a>
+            <a class="navbar-item" href="/mars_api">Home</a>
+            <a class="navbar-item" href="">Add Game</a>
             <a class="navbar-item" href="">Games</a>
+            <a class="navbar-item" href="">Player Stats</a>
+            <a class="navbar-item" href="">About</a>
         </div>
     </div>
   </nav>
@@ -26,6 +29,17 @@
 
 /*})*/
 export default {
+methods: {
+    toggleBurger: function(event) {
+        const navbarTarget = event.target.dataset.target;
+        const elTarget = document.getElementById(navbarTarget);
 
+        if (elTarget != null) {
+            event.target.classList.toggle("is-active");
+            elTarget.classList.toggle("is-active");
+        }
+        else console.error("Couldn't find navbar element.");
+    }
+ }
 };
 </script>
