1644401c971022410ffbc869e75f309b937ee51f
RokasBagdonas <rokius.bagdonas@gmail.com>
2020-10-02 12:38:31 +0100

    Modify Vue/Django routing
    Direct all routes that except for `mars_api` to the frontend app. That
    makes Vue the main router.

--- mars/urls.py
+++ mars/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("mars_api/", include("mars_api.urls")),
-    path("", include("frontend.urls")),
+    re_path(r"^.*$", include("frontend.urls")),
 ]
