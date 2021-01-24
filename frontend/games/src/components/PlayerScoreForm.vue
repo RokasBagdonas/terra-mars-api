e644c7a1d29b57f9d3eb5495a603eead1a93f8cb
RokasBagdonas <rokius.bagdonas@gmail.com>
2021-01-24 08:34:37 +0000

    form prototype

---
+++ frontend/games/src/components/PlayerScoreForm.vue
@@ -0,0 +1,25 @@
+<template>
+  <h5>player score form</h5>
+  <p>{{ playerScore}}</p>
+
+  <BaseInput v-model="playerScore.terraform_rating" label="TR"/>
+</template>
+
+
+<script lang="ts">
+import BaseInput from "./form-components/BaseInput.vue";
+import { PlayerScore } from "../classes";
+import { PropType } from "vue";
+
+export default {
+  components: {
+    BaseInput,
+  },
+  props: {
+    playerScore: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
--- frontend/urls.py
+++ frontend/urls.py
@@ -5,4 +5,5 @@ from frontend.views import IndexView
 urlpatterns = [
     path("", IndexView.as_view()),
     path("games/", IndexView.as_view()),
+    path("add_game/", IndexView.as_view()),
 ]
