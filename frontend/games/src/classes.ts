42a3d33853761c6d9ebd772a723f29816ca77dbe
RokasBagdonas <rokius.bagdonas@gmail.com>
2021-01-24 08:34:37 +0000

    form prototype

---
+++ frontend/games/src/classes.ts
@@ -0,0 +1,25 @@
+export class PlayerScore{
+  terraform_rating: number;
+  corporation: string;
+  //player: Player,
+  //terraform_rating: "TR",
+  //milestones: "Milestones",
+  //awards: "Awards",
+  //greeneries: "Greeneries",
+  //cities: "Cities",
+  //event_cards: "Events",
+  //automated_cards: "Automoated cards",
+  //active_cards: "Active cards",
+  //resources: "Resources",
+  //total_score: "Final Score",
+  //is_winner: "Won?",
+  //id: "score_id",
+  //game_id: "game_id"
+
+  constructor(){
+    this.terraform_rating = 20;
+    this.corporation = "";
+  }
+
+}
+
---
+++ frontend/games/src/views/AddGame.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Add Game</h1>
+  <p>AddGame TR: {{score}}</p>
+  <PlayerScoreForm :playerScore="score"/>
+</template>
+
+
+<script>
+import PlayerScoreForm from "../components/PlayerScoreForm";
+import { PlayerScore } from "../classes";
+
+export default {
+  components: {
+    PlayerScoreForm,
+  },
+  data() {
+    return {
+      scores: [new PlayerScore()],
+      score: new PlayerScore(),
+    };
+  },
+};
+</script>
