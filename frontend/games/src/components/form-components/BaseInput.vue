46bc79ef58a539096bee386a6d3cd0b02ebfec06
RokasBagdonas <rokius.bagdonas@gmail.com>
2021-01-26 06:47:33 +0000

    improve Form styling

--- frontend/games/src/components/form-components/BaseInput.vue
+++ frontend/games/src/components/form-components/BaseInput.vue
@@ -1,12 +1,15 @@
 <template>
-  <label v-if="label"> {{ label }}</label>
-  <input
-    :placeholder="label"
-    class="input"
-    :value="modelValue"
-    v-bind="$attrs"
-    @input="$emit('update:modelValue', $event.target.value)"
-  />
+  <div class="field">
+    <label class="label" v-if="label"> {{ label }}</label>
+    <div class="control">
+    <input
+      :placeholder="label"
+      class="input"
+      :value="modelValue"
+      v-bind="$attrs"
+      @input="$emit('update:modelValue', $event.target.value)"
+    /></div>
+  </div>
 </template>
 
 
